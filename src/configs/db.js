e57cfb47d3f300dcd8547235c46000aa1ebf77d8
Nomad <noneprado@gmail.com>
2025-02-19 14:25:24 +0600

    fixed mongodb connection string bug

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,32 +1,36 @@
 require('dotenv').config({ path: '../.env' });
 const mongoose = require('mongoose');
 
-const { getOptionUri, getConnectionUri } = require('../utils/helper');
+const { getOptionUri } = require('../utils/helper');
+
 const env = process.env.ENV;
-const mongoPwd = process.env.MONGO_PWD;
+const dbUri = process.env.MONGO_DB;
 const mongoUser = process.env.MONGO_USER;
+const mongoPwd = process.env.MONGO_PWD;
 
 const pwd = mongoPwd.replace(/@/g, '\\@');
-const userUri = `${mongoUser}:${pwd}`;
-const hostUri = getConnectionUri();
+const host = process.env.HOST;
 const port = process.env.MONGO_PORT;
-const dbUri = process.env.MONGO_DB;
+
+const userUri = `${mongoUser}:${pwd}`;
+const hostUri = `${host}:${port}`;
+
 const options = getOptionUri();
 
 let mongoConnectionUri;
 
-let mongooseConfig = {
+const mongooseConfig = {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
 };
 
-if (env === 'production') {
-	mongoConnectionUri = `mongodb://${userUri}@${hostUri}/${dbUri}?${options}`;
-	console.log('This project is running in production mode');
-} else {
+if (env === 'development') {
 	mongoConnectionUri = `mongodb://${hostUri}/${dbUri}`;
 	console.log('This project is running in development mode');
+} else {
+	mongoConnectionUri = `mongodb://${userUri}@${hostUri}/${dbUri}?${options}`;
+	console.log('This project is running in production mode');
 }
 
 //Configure mongoose's promise to global promise
@@ -35,7 +39,7 @@ mongoose.promise = global.Promise;
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(mongoConnectionUri, mongooseConfig);
-		console.log(`MongoDB Connected: ${conn.connection.host}:${port}`);
+		console.log(`MongoDB started at ${conn.connection.host}:${port}`);
 	} catch (err) {
 		console.log(`MongoDB connection error ${err}`);
 	}
